b8b16019a53a4a233b17ae65350d91d3f8147b1c
fona-bogdan <bogdan.fona@student.upt.ro>
2026-04-14 12:28:45 +0300

    feat: conectare la AWS RDS si trimitere date accelerometru in cloud

--- apps/mobile/src/services/api.js
+++ apps/mobile/src/services/api.js
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE_URL = 'http://10.234.21.79:3001'; // Android emulator -> localhost
+const API_BASE_URL = 'http://192.168.1.109:3001';; // Android emulator -> localhost
 
 const getHeaders = async () => {
   const patientId = await AsyncStorage.getItem('patient_id') || '1';

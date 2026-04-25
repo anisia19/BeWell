6fef4b1e620eb9a8dc4e7e434e85690afe603a16
anisia19 <anisia.fort19@gmail.com>
2026-04-25 14:26:58 +0300

    Add heading and button to patients view page

--- apps/web/src/pages/Patients.tsx
+++ apps/web/src/pages/Patients.tsx
@@ -1,7 +1,27 @@
+import { Heading, Text, Button } from "@chakra-ui/react";
 import "../index.css";
-
+import "./Patients.css";
 function Patients() {
-  return <div>Patients page</div>;
+  return (
+    <>
+      <div className="patients-page">
+        <div className="patients-header-row">
+          <Heading as="h3" size="md">
+            Patients
+          </Heading>
+          <Button
+            variant="solid"
+            colorScheme="green"
+            onClick={() => console.log("Button clicked")}
+          >
+            <i className="bi bi-person-add"></i>
+            Add Patient
+          </Button>
+        </div>
+        <Text fontSize="xs">X total patients</Text>
+      </div>
+    </>
+  );
 }
 
 export default Patients;
